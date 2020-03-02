3ca491f06ed7ed7adcf9e4bd5318c9099afe2e17
Pranav V R <pvrforpranavvr@gmail.com>
2020-03-02 17:57:50 +0530

    Initial Commit

---
+++ src/main/java/com/pvr/bdgs/BigDataGeneratorSuiteApplication.java
@@ -0,0 +1,13 @@
+package com.pvr.bdgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigDataGeneratorSuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigDataGeneratorSuiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pvr/bdgs/BigDataGeneratorSuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvr.bdgs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigDataGeneratorSuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
